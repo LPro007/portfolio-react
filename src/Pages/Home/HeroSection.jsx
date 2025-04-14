cfe6d4df6a25bf32a292f4b5923bab113543fbbb
Daniel Brambila <danielbrambila@Daniels-MacBook-Pro.local>
2025-04-14 16:03:46 -0700

    Portfolio update

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Daniel Brambila</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Frontend</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            With over six years of experience as a Front-end Developer, 
+            I’m passionate about creating intuitive UI/UX that users love. 
+            My expertise in the leading frameworks React and Angular allows me to create high performing, high quality web systems.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
